a4152cbd7ac4fbba03d5772ba4257ff51c99d879
aero127 <arnoldvdijk@hotmail.com>
2021-06-08 15:47:38 +0200

    backup eindopdracht

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/Button.css';
+import './components/Header.css';
+import './components/Footer.css';
+import './pages/Home.css';
+import './components/Photoslider.css';
+import './pages/Login.css'
+import './pages/LostPassword.css'
+import Header from './components/Header';
+import backgroundtop from './assets/background_topcut.jpg';
+import mtblogo from './assets/mtbrental.png';
+import Footer from './components/Footer';
+import background_footer from './assets/background_footer.jpg';
+import Home from './pages/Home';
+import Login from './pages/Login'
+import LostPassword from "./pages/LostPassword";
+import Photoslider from "./components/Photoslider";
+import Registration from './pages/Registration'
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header
+      background={backgroundtop}
+      mtblogo={mtblogo}
+      />
+        <Login/>
+        <Home/>
+        <LostPassword/>
+        <Registration/>
+      <Footer
+          background_footer={background_footer}
+      />
+
     </div>
   );
 }
